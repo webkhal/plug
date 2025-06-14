cb925e192ae7c88c15235b682e7fffa9286c17c3
webkhal <khalidcodes034@gmail.com>
2025-06-15 04:16:05 +0530

    Updated : fixed  errors

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,13 +1,15 @@
+import Link from 'next/link';
+
 export default function ProductsLandingPage() {
   return (
     <div style={{ padding: '2rem' }}>
       <h1>All Product Categories</h1>
       <ul>
-        <li><a href="/products/buttons">Buttons</a></li>
-        <li><a href="/products/footwear">Footwear</a></li>
-        <li><a href="/products/kurtis">Kurtis</a></li>
-        <li><a href="/products/watches">Watches</a></li>
-        <li><a href="/products/abayas">Abayas</a></li>
+        <li><Link href="/products/buttons">Buttons</Link></li>
+        <li><Link href="/products/footwear">Footwear</Link></li>
+        <li><Link href="/products/kurtis">Kurtis</Link></li>
+        <li><Link href="/products/watches">Watches</Link></li>
+        <li><Link href="/products/abayas">Abayas</Link></li>
       </ul>
     </div>
   );
